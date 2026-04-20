003b9ef22938a3ceb54593e4292cefbf5ca30fbc
Yuvraj <yuvraj@fynancial.com>
2026-04-20 15:01:13 +0530

    added libraries

--- index.js
+++ index.js
@@ -2,8 +2,77 @@
  * @format
  */
 
+import notifee, { EventType } from '@notifee/react-native';
+import {
+  getMessaging,
+  setBackgroundMessageHandler,
+} from '@react-native-firebase/messaging';
 import { AppRegistry } from 'react-native';
-import App from './App';
+import 'react-native-url-polyfill/auto'; //needed for signal R
 import { name as appName } from './app.json';
+import App from './src/App';
+import { onMessageReceived } from './src/utils/notificationUtils';
 
-AppRegistry.registerComponent(appName, () => App);
+if (!AbortSignal.prototype.throwIfAborted) {
+  AbortSignal.prototype.throwIfAborted = function () {
+    if (this.aborted) {
+      const error = new Error('This operation was aborted.');
+      error.name = 'AbortError';
+      throw error;
+    }
+  };
+}
+
+if (!AbortSignal.any) {
+  AbortSignal.any = function (signals) {
+    const controller = new AbortController();
+
+    function onAbort() {
+      controller.abort();
+      signals.forEach(s => s.removeEventListener('abort', onAbort));
+    }
+
+    for (const signal of signals) {
+      if (signal.aborted) {
+        controller.abort();
+        break;
+      }
+      signal.addEventListener('abort', onAbort);
+    }
+
+    return controller.signal;
+  };
+}
+
+const messaging = getMessaging();
+
+// background notification event listener
+notifee.onBackgroundEvent(async ({ type, detail }) => {
+  const { notification, pressAction } = detail;
+
+  // Check if the user pressed the "Mark as read" action
+  if (type === EventType.ACTION_PRESS) {
+    //await notifee.cancelNotification(notification.id);
+  }
+});
+
+// receive background notification
+setBackgroundMessageHandler(messaging, async message => {
+  //if (userStore?.getState?.()?.userDetails?.userID) {
+  onMessageReceived(message, 'background');
+  //}
+});
+
+// Check if app was launched in the background and conditionally render null if so
+function HeadlessCheck({ isHeadless }) {
+  if (isHeadless) {
+    // App has been launched in the background by iOS, ignore
+    return null;
+  }
+
+  // Render the app component on foreground launch
+  return <App />;
+}
+
+//AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => HeadlessCheck);
--- upgrade.md
+++ upgrade.md
@@ -1,2 +1,16 @@
 1. node update above >= 22.11.0
 2. yarn update as per node => "zsh: command not found: yarn"
+3.
+
+compatibility issues
+
+1. React-Native-Firebase !==> Flipper & Fabric. //we cant use both together compatibility issue
+
+IF WONT RUN FIX THIS
+
+1. copy pasted the tsconfig.json
+2. COPY PASTE -> react-native.config.js
+
+bhai how can i make the files for firebase with "FynancialRN85" this name, now i want package name and bundle id to be ---> "com.fynancialrn85"
+
+if somehow i get into the
