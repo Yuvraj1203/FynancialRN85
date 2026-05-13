4d4270dc81d08003694a07ac3825de74d39b7d34
Yuvraj <yuvrajrathore1203@gmail.com>
2026-05-13 10:50:43 +0530

    swift fix and library update

--- ios/Developers/AppDelegate.swift
+++ ios/Developers/AppDelegate.swift
@@ -34,6 +34,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     return true
   }
+
+  func application(
+    _ app: UIApplication,
+    open url: URL,
+    options: [UIApplication.OpenURLOptionsKey : Any] = [:]
+  ) -> Bool {
+    return true
+  }
 }
 
 class ReactNativeDelegate: RCTDefaultReactNativeFactoryDelegate {
